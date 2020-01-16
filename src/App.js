c40594c705d78426d89c1eda6483bdd64688c0d3
Michael Padiernos <mike.padiernos@gmail.com>
2020-01-15 22:29:15 -0500

    FEATURE:(Task & TodoList): Initial code for Task & TodoList
    - started code for Task
    - started code for TodoList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Data from "./Data/Data";
 
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Task = props => {
+	return (
+		<article
+			// onClick={event => props.toggleCompleted(props.item.id)}
+			// className={`item${props.item.completed ? " completed" : ""}`}
+		>
+			<p>{props.item.task}</p>
+		</article>
+	);
+};
+
+export default Task;
