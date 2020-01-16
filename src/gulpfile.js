277748f3cd9c10e08c6916f94cb3e818f294b14f
Michael Padiernos <mike.padiernos@gmail.com>
2020-01-16 12:29:42 -0500

    STYLE:(App): "Minimal" styling applied
    - coded overall app in minimalism style
    - coded flat style input & buttons

---
+++ src/gulpfile.js
@@ -0,0 +1,23 @@
+const nano     = require( 'cssnano' ),
+			gulp        = require( 'gulp' ),
+			stylus      = require( 'gulp-stylus' ),
+			postcss     = require( 'gulp-postcss' );
+
+
+gulp.task('app', () => {
+
+	const processors = [
+		nano
+	];
+
+	return gulp.src('./styl/App.styl')
+		.pipe(stylus({
+			// use: [nib(), rupture()]
+		}))
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', () => {
+	gulp.watch('**/*.styl',  gulp.series('app'));
+});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import "./Components/Todo/Todo.css"
+import "./css/App.css";
 
 render(<App />, document.getElementById('root'));
