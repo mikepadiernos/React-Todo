78aa941d41ccbc70881f6cb541e5b0619080a24d
Michael Padiernos <mike.padiernos@gmail.com>
2020-01-15 22:17:41 -0500

    BUILD:(App & Data): Imports & renders
    - created & imported data array from Data.js
    - coded semantic code for rendering the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import Data from "./Data/Data";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: Data,
+      z: ""
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+      <main>
+        <header>
+          <h1>Todo App</h1>
+        </header>
+        <main>
+
+        </main>
+      </main>
     );
   }
 }
